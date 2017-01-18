8e036d93c8642073b162a96cb4a5a607b7892856
Eli Anderson <eli@theprogrampdx.com>
2017-01-17 19:10:43 -0800

    Whitespace police

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -4,6 +4,7 @@ class OrganizationTest < ActiveSupport::TestCase
 
   test 'must have a name' do
     organization = Organization.new
+
     refute organization.valid?, 'organization is valid without a name'
     assert_not_nil organization.errors[:name], 'no validation for name present'
   end
