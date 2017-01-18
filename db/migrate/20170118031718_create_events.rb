5cfb5c9415a3897cbd57f2c078a3bf242b3b1ca9
Eli Anderson <eli@theprogrampdx.com>
2017-01-17 19:24:42 -0800

    Edit spelling error in migration

--- db/migrate/20170118031718_create_events.rb
+++ db/migrate/20170118031718_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-      t.interger :organization_id
+      t.integer :organization_id
       t.string :message
       t.string :hostname
       t.datetime :timestamp
