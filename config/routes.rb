f1af92e828cb7a7b6cd2a01b90f9b070bed9b8cf
Eli Anderson <eli@theprogrampdx.com>
2017-01-17 18:42:04 -0800

    remove possible senative files from the repository and replace with example files
    Add the schema file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :organizations
+  resources :organizations, except: [:update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
