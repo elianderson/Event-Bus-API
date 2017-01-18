fb61ec250d57e6e8ff039be24ca1f41c9e6e62b8
Eli Anderson <eli@theprogrampdx.com>
2017-01-17 19:41:59 -0800

    Get some basic validation on an Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+  validates :organization_id, :message, :hostname, :timestamp, presence: true
+
+  default_scope { order('id desc') }
+
   belongs_to :organization
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'must have a event_id, message, hostname and timestamp' do
+    event = Event.new
+
+    refute event.valid?, 'event is invalid'
+  end
+
+  test 'should list the newest first' do
+    event = Event.create(organization_id: 1,
+                          message: 'Important Message',
+                          hostname: 'www.importantplace.com',
+                          timestamp: Time.now)
+
+    assert_equal event, Event.first
+  end
 end
