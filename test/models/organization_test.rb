02b498c0023abf68dffb7302634b1bc3816745f1
Eli Anderson <eli@theprogrampdx.com>
2017-01-17 18:43:09 -0800

    Make the Organization name required

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'must have a name' do
+    organization = Organization.new
+    refute organization.valid?, 'organization is valid without a name'
+    assert_not_nil organization.errors[:name], 'no validation for name present'
+  end
+
 end
