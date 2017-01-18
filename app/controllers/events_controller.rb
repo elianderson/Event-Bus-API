b857dcc4c58b8f537e2814d6e385e0316dd8d3e3
Eli Anderson <eli@theprogrampdx.com>
2017-01-17 19:46:44 -0800

    Create an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,17 @@
 class EventsController < ApplicationController
+
+  def create
+    organization = Organization.find_by_name(params[:organization_id])
+
+    Event.create(event_params)
+
+    render json: organization.events.to_json
+
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:message, :hostname, :timestamp)
+  end
 end
--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "an event can created" do
+    original_event_count = Event.count
+
+    event_params = {event: {
+                    message: 'Important Message',
+                    hostname: 'www.importantplace.com',
+                    timestamp: Time.now
+    }}
+
+    post organization_events_url(organization_id: Organization.first.name), params: event_params
+
+    assert_response :success
+    assert_equal original_event_count + 1, Event.count
+  end
+
 end
